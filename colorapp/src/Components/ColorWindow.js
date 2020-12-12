8aeb3dfb5b1d4930a106bfbdf23a19333d27c8ce
AlwaysBlind <ulfbaeltdjur@gmail.com>
2020-12-12 15:53:15 +0100

    Add more advanced logic

--- colorapp/src/Components/ColorWindow.js
+++ colorapp/src/Components/ColorWindow.js
@@ -1,12 +1,31 @@
 import { Button, ButtonGroup } from "react-bootstrap";
 const usableColors = ['green', 'blue', 'red', 'orange', 'black', 'white', 'purple', 'pink', 'yellow']
+const colorDict = { 'white': 0, "yellow": 2, "orange": 4, "red": 5, "pink": 6, "purple": 8, "blue": 10, "green": 12, "black": 14 }
+const numberToColorDict = {}
+Object.entries(colorDict).forEach(([color, number], _) => {
+    numberToColorDict[number] = color
+});
+
+const illegalNeighbors = {}
+Object.entries(numberToColorDict).forEach(([number, color], _) => {
+    illegalNeighbors[color] = new Set()
+    if (numberToColorDict[number - 1]) {
+        illegalNeighbors[color].add(numberToColorDict[number - 1])
+    }
+    if (numberToColorDict[+number + +1]) {
+        illegalNeighbors[color].add(numberToColorDict[+number + +1])
+    }
+});
+
 
 const ColorWindow = ({ subArray, addColor }) => {
-    const unionOfColors = new Set(subArray.reduce((a, b) => a.concat([...b]), []));
-    console.log(unionOfColors)
-    const filteredColors = [...usableColors].filter(x => !unionOfColors.has(x))
+    const subArrayColors = new Set(subArray.reduce((a, b) => a.concat([...b]), []));
+    const illegalNeighborsSubArray1 = [...subArrayColors].map(col => illegalNeighbors[col])
+    const illegalNeighborsSubArray = new Set([...illegalNeighborsSubArray1].reduce((a, b) => a.concat([...b]), []));
+    const illegalColors = new Set([...subArrayColors, ...illegalNeighborsSubArray])
+    const filteredColors = [...usableColors].filter(x => !illegalColors.has(x))
     const colorMap = () => {
-        const mappedCols = [...filteredColors].map((color, index) => <Button key={index} style={{ 'background-color': color }
+        const mappedCols = [...filteredColors].map((color, index) => <Button key={index} style={{ 'backgroundColor': color }
         } onClick={() => { addColor(color) }}>  </Button >)
         return <ButtonGroup vertical> {mappedCols} </ButtonGroup>
     }
